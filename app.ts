a90b02c813b67697f124749b0fab3ee52d385724
baegofda <molly.daleo3o@gmail.com>
2021-12-11 21:53:46 +0900

    feat: ts 전환 완료

--- app.ts
+++ app.ts
@@ -1,31 +1,72 @@
-const container = document.getElementById("root");
-const ajax = new XMLHttpRequest();
+interface Store {
+  currentPage: number;
+  feeds: NewsFeed[];
+}
+
+interface News {
+  readonly id: number;
+  readonly url: string;
+  readonly user: string;
+  readonly time_ago: string;
+  readonly title: string;
+  readonly content: string;
+}
+
+// 인터섹션 기능
+interface NewsFeed extends News {
+  readonly comments_count: number;
+  readonly points: number;
+  read?: boolean;
+}
+
+interface NewsDetail extends News {
+  readonly comments: NewsComment[];
+}
+
+interface NewsComment extends News {
+  readonly comments: NewsComment[];
+  level: number;
+}
+
+const container: HTMLElement | null = document.getElementById("root");
+const ajax: XMLHttpRequest = new XMLHttpRequest();
 const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
-const store = {
+const store: Store = {
   currentPage: 1,
   feeds: [],
 };
 
 // fetch
-function getData(url) {
+// 제너릭 : 사용되는 곳에서 타입을 명시하면 그것을 사용하게 됨
+function getData<T>(url: string): T {
   ajax.open("GET", url, false);
   ajax.send();
   return JSON.parse(ajax.response);
 }
 
-function makeFeeds(feeds) {
-  for (let i = 0; i < feeds.lenght; i++) {
+// 과거에 읽은 글인지 체크하기위함
+function makeFeeds(feeds: NewsFeed[]): NewsFeed[] {
+  for (let i = 0; i < feeds.length; i++) {
     feeds[i].read = false;
   }
 
   return feeds;
 }
 
+// 타입가드 코드
+function updateView(html: string): void {
+  if (container) {
+    container.innerHTML = html;
+  } else {
+    console.error("최상위 컨테이너가 없어서 UI 작업진행이 불가능합니다.");
+  }
+}
+
 // 목록 화면
-function newsFeed() {
-  let newsFeed = store.feeds;
+function newsFeed(): void {
+  let newsFeed: NewsFeed[] = store.feeds;
   const newsList = [];
   let template = `
   <div class="bg-gray-600 min-h-screen">
@@ -47,7 +88,8 @@ function newsFeed() {
   `;
 
   if (newsFeed.length === 0) {
-    newsFeed = store.feeds = makeFeeds(getData(NEWS_URL));
+    // 제너릭
+    newsFeed = store.feeds = makeFeeds(getData<NewsFeed[]>(NEWS_URL));
   }
 
   // 현재 1페이지를 보고있으면 배열의 0부터 시작해야하니 -1
@@ -81,22 +123,24 @@ function newsFeed() {
   template = template.replace("{{__news_feed__}}", newsList.join(""));
   template = template.replace(
     "{{__prev_page__}}",
-    store.currentPage > 1 ? store.currentPage - 1 : 1
+    String(store.currentPage > 1 ? store.currentPage - 1 : 1)
   );
   template = template.replace(
     "{{__next_page__}}",
-    Math.ceil(newsFeed.length / 10) > store.currentPage
-      ? store.currentPage + 1
-      : store.currentPage
+    String(
+      Math.ceil(newsFeed.length / 10) > store.currentPage
+        ? store.currentPage + 1
+        : store.currentPage
+    )
   );
 
-  container.innerHTML = template;
+  updateView(template);
 }
 
 // 내용화면
-function newsDetail() {
+function newsDetail(): void {
   const id = location.hash.substring(7);
-  const newsContent = getData(CONTENT_URL.replace("@id", id));
+  const newsContent = getData<NewsDetail>(CONTENT_URL.replace("@id", id));
   let template = `
     <div class="bg-gray-600 min-h-screen pb-8">
       <div class="bg-white text-xl">
@@ -130,34 +174,34 @@ function newsDetail() {
     }
   }
 
-  function makeComment(comments, called = 0) {
-    const commentString = [];
-
-    for (let i = 0; i < comments.length; i++) {
-      commentString.push(`
-        <div style="padding-left: ${called * 40}px;" class="mt-4">
-          <div class="text-gray-400">
-            <i class="fa fa-sort-up mr-2"></i>
-            <strong>${comments[i].user}</strong> ${comments[i].time_age}
-          </div>
-          <p class="text-gray-700">${comments[i].content}</p>
-        </div>
-      `);
-      if (comments[i].comments.length > 0) {
-        commentString.push(makeComment(comments[i].comments, called + 1));
-      }
-    }
-
-    return commentString.join("");
-  }
-
-  container.innerHTML = template.replace(
-    "{{__comments__}}",
-    makeComment(newsContent.comments)
+  updateView(
+    template.replace("{{__comments__}}", makeComment(newsContent.comments))
   );
 }
 
-function router() {
+function makeComment(comments: NewsComment[]): string {
+  const commentString = [];
+
+  for (let i = 0; i < comments.length; i++) {
+    const comment: NewsComment = comments[i];
+    commentString.push(`
+      <div style="padding-left: ${comment.level * 40}px;" class="mt-4">
+        <div class="text-gray-400">
+          <i class="fa fa-sort-up mr-2"></i>
+          <strong>${comment.user}</strong> ${comment.time_ago}
+        </div>
+        <p class="text-gray-700">${comment.content}</p>
+      </div>
+    `);
+    if (comment.comments.length > 0) {
+      commentString.push(makeComment(comment.comments));
+    }
+  }
+
+  return commentString.join("");
+}
+
+function router(): void {
   const routePath = location.hash;
 
   //hash에 #만 들어가있으면 빈값을 반환하기때문에 !!
